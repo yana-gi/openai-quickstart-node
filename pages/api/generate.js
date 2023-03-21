ae3b54d483970d002f55b69d93eb40bc65935244
yana-gi <yanagisawa.hika@gmail.com>
2023-03-21 19:26:13 +0900

    プロンプトを日本語に変えると結果も日本語になる

--- pages/api/generate.js
+++ pages/api/generate.js
@@ -53,10 +53,10 @@ function generatePrompt(animal) {
     animal[0].toUpperCase() + animal.slice(1).toLowerCase();
   return `Suggest three names for an animal that is a superhero.
 
-Animal: Cat
-Names: Captain Sharpclaw, Agent Fluffball, The Incredible Feline
-Animal: Dog
-Names: Ruff the Protector, Wonder Canine, Sir Barks-a-Lot
+Animal: 猫
+Names: たま、 にゃんこ、 ニャース
+Animal: 犬
+Names: ポチ、 ココ、むぎ
 Animal: ${capitalizedAnimal}
 Names:`;
 }
